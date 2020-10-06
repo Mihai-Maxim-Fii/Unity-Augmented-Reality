c003b087be3ab7bc433cf611db632adb59593232
Mihai-Maxim-Fii <mihai.maximfii@gmail.com>
2020-10-06 21:32:49 +0300

    done

---
+++ Assets/Scripts/AttackScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackScript : MonoBehaviour
+{
+  
+    void Start()
+    {
+        GetComponent<Animator>().SetBool("AttackMe", false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float d = Vector3.Distance(Camera.main.transform.position, transform.position);
+           if(d<10.50f)
+            {
+                GetComponent<Animator>().SetBool("AttackMe", true);
+            }
+           else
+                GetComponent<Animator>().SetBool("AttackMe", false);
+    }
+}
